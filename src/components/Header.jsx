b09392caf4082ab6744498253dfe0ec6cad9cc1d
Nikita-Minov <nikitachryackhov555@gmail.com>
2021-03-05 16:06:25 +0300

    Added css modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import s from "./Header.module.css";
+
 const Header = () => {
   return (
-    <header className='header' >
+    <header className={s.header} >
         <img src="https://www.digitaladnepal.com/wp-content/uploads/2020/05/huwai-logo.jpg" />
     </header>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,8 @@
+import s from "./Profile.module.css"
+
 const Profile = () => {
   return (
-     <div className='content' >
+     <div className={s.content} >
         <div>
            <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
         </div>
@@ -13,11 +15,13 @@ const Profile = () => {
             New post
           </div>
         </div>
-        <div>
-          post 1
-        </div>
-        <div>
-          post 2
+          <div className={s.posts} >
+            <div className={s.item} >
+            post 1
+          </div>
+          <div className={s.item}>
+            post 2
+          </div>
         </div>
       </div>
   );
