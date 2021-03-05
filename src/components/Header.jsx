a4cee6b43be5f0305de9036b08f2c6d29b17fef8
Nikita-Minov <nikitachryackhov555@gmail.com>
2021-03-05 15:01:58 +0300

    Create desin

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className='header' >
+        <img src="https://www.digitaladnepal.com/wp-content/uploads/2020/05/huwai-logo.jpg" />
+    </header>
+  );
+}
+
+export default Header;
